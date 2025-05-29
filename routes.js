3ac181509b7d1f25f6ecaca4753e2b08b8f36e1e
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-05-29 15:50:21 +0530

    Routes

---
+++ routes.js
@@ -0,0 +1,46 @@
+// Route Handlers
+
+const http = require("http");
+const url = require("url");
+
+
+
+// Handling Multiple Routes
+
+const routes = {
+    '/': (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Welcome to the Home Page!");
+    },
+    '/about': (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Welcome to the About Page!");
+    },
+    '/contact': (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Welcome to the Contact Page!");
+    },
+    '/blogs': (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Welcome to the Blogs Page!");
+    },
+    '/notfound': (req, res) => {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Page Not Found");
+    }
+}
+
+const server = http.createServer((req, res) => {
+    const { pathname } = url.parse(req.url);
+    if(routes[pathname]) {
+        routes[pathname](req, res);
+    } else {
+        routes['/notfound'](req, res);
+    }
+});
+
+
+
+server.listen(3000, () =>{
+    console.log(`Server is running on http://localhost:3000`);
+})
