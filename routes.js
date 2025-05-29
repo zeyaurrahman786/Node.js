a26863c6248122f60c5fc144f3f7d8d823e76339
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-05-29 16:14:29 +0530

    Routes

--- routes.js
+++ routes.js
@@ -7,37 +7,61 @@ const url = require("url");
 
 // Handling Multiple Routes
 
-const routes = {
-    '/': (req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Welcome to the Home Page!");
-    },
-    '/about': (req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Welcome to the About Page!");
-    },
-    '/contact': (req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Welcome to the Contact Page!");
-    },
-    '/blogs': (req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Welcome to the Blogs Page!");
-    },
-    '/notfound': (req, res) => {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end("Page Not Found");
-    }
-}
+// const routes = {
+//     '/': (req, res) => {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end("Welcome to the Home Page!");
+//     },
+//     '/about': (req, res) => {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end("Welcome to the About Page!");
+//     },
+//     '/contact': (req, res) => {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end("Welcome to the Contact Page!");
+//     },
+//     '/blogs': (req, res) => {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end("Welcome to the Blogs Page!");
+//     },
+//     '/notfound': (req, res) => {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.end("Page Not Found");
+//     }
+// }
+
+// const server = http.createServer((req, res) => {
+//     const { pathname } = url.parse(req.url);
+//     if(routes[pathname]) {
+//         routes[pathname](req, res);
+//     } else {
+//         routes['/notfound'](req, res);
+//     }
+// });
+
+
+
+
+
+
+
+
+
+// Handling Dynamic Routes
 
 const server = http.createServer((req, res) => {
     const { pathname } = url.parse(req.url);
-    if(routes[pathname]) {
-        routes[pathname](req, res);
+    if(pathname.startsWith('/products/')){
+        const items = pathname.split('/')[2];  '/products/item1'
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(`Welcome to the Products Page! You are viewing: ${items}`);
     } else {
-        routes['/notfound'](req, res);
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Page Not Found");
     }
-});
+})
+
+
 
 
 
